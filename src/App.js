0f58665bee213a72f86ff43cc48bdac4809d3761
Ying Feng <ccfengying@gmail.com>
2021-07-21 23:36:09 +0100

    add static visual mockup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">Image Rotator</div>
+      <div className="container">
+      <div className="sidebar">
+        <div className="upload-area">
+          <div className="upload-btn">Upload Image</div>
+        </div>
+        <div className="file-area">
+          <div className="file-text">File: </div>
+          <div className="file-text">Width: </div>
+          <div className="file-text">Height: </div>
+          <div className="file-text">Rotate:
+            <input type="number" className="rotate-input"></input>
+            <span className="rotate-btn">Apply</span>
+          </div>
+        </div>
+      </div>
+      <div className="main">
+        <div className="timer">rendered in 3.562ms</div>
+        <canvas className="canvas-area"></canvas>
+      </div>
+      </div>
     </div>
   );
 }

aec3c8ce5e6639e502c3c6f0c344507f1029ff19
Ying Feng <ccfengying@gmail.com>
2021-07-25 19:49:07 +0100

    fix invalid angle input in src/components/Panel.js

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -9,10 +9,24 @@ function Panel (props) {
     angleRef.current.value = 0
   }, [file])
 
+  const checkInput = (event) => {
+    // negative sign (45), decimal separator(46) and [0-9] (48-57) are valid inputs
+    if (!(event.charCode === 45 || event.charCode === 46 || (event.charCode >= 48 && event.charCode <= 57))) {
+      event.preventDefault()
+      event.target.value = ''
+    }
+  }
+
   const changeAngle = (event) => {
     event.preventDefault()
-    const val = parseInt(angleRef.current.value) || 0 // should check for valid number first
-    return callback(val)
+    const val = angleRef.current.valueAsNumber
+    if (!isNaN(val)) {
+      return callback(val)
+    } else {
+      // should we reset rotation for invalid input?
+      angleRef.current.value = 0
+      return callback(0)
+    }
   }
 
   return (
@@ -21,10 +35,12 @@ function Panel (props) {
     <div className={panelText}>Width: {file.width && `${file.width} px`} </div>
     <div className={panelText}>Height: {file.height && `${file.height} px`} </div>
     <div className={panelText}>Rotate:
-      <input type="number"
-        name="angle"
+      <input name="angle"
+        type="number"
         className={rotateInput}
-        ref={angleRef} />
+        ref={angleRef}
+        onKeyPress={checkInput}
+      />
       <label htmlFor="angle"
         className={rotateBtn}
         onClick={changeAngle}>Apply
